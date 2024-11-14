a4d1ccf24e76ca788e6b6635fd1f1f4598ca7ada
DevMon724 <devmon724@gmail.com>
2024-11-14 13:44:16 +0900

    ci.yml 수정

--- temp.py
+++ temp.py
@@ -5,3 +5,8 @@ my_dict: dict[str, int] = {"a": 1}
 my_tuple: tuple[int, str, int] = (1, "hi", 3)
 
 my_tuple2: tuple[int, ...] = (1, 2, 3, 4)  # 길이는 모르겠지만 요소는 다 int 형이야.
+
+
+import os
+
+print(os.environ.get("temp", "없음"))
