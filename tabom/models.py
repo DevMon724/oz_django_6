911c8196e37aa13a64452bae20c60ece5c5a018a
DevMon724 <devmon724@gmail.com>
2024-11-14 14:50:04 +0900

    implement do_like.py

--- tabom/models.py
+++ tabom/models.py
@@ -3,20 +3,23 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
+# 상속클래스이기 때문에 데이터베이스 모델에는 변경사항 없어서 migration 되지 않음.
+class BaseModel(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel):
     name = models.CharField(max_length=50)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Article(models.Model):
+class Article(BaseModel):
     title = models.CharField(max_length=255)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Like(models.Model):
+class Like(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
