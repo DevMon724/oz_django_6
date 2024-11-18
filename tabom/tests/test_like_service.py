2c15786c60f17908a0a513d237148738a081685c
DevMon724 <devmon724@gmail.com>
2024-11-18 11:18:42 +0900

    단건 조회기능 구현

--- tabom/tests/test_like_service.py
+++ tabom/tests/test_like_service.py
@@ -1,8 +1,8 @@
 from django.db.utils import IntegrityError
 from django.test import TestCase
 
-from tabom.models import Article, User
-from tabom.sevices.like_service import do_like
+from tabom.models import Article, Like, User
+from tabom.services.like_service import do_like, undo_like
 
 
 class TestLikeService(TestCase):
@@ -33,3 +33,46 @@ class TestLikeService(TestCase):
         ):  # exception이 발생하면 통과, 아무일도 안일어나면 AssertionError를 일으킨다.
 
             like2 = do_like(user.id, article.id)
+
+    def test_it_should_raise_exception_when_like_an_user_does_not_exist(self) -> None:
+        # Given
+        invalid_user_id = 9988
+        article = Article.objects.create(title="test_title")
+
+        # Expect
+        with self.assertRaises(IntegrityError):
+            do_like(invalid_user_id, article.id)
+
+    def test_it_should_raise_exception_when_like_an_article_does_not_exist(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        invalid_article_id = 9988
+
+        # Expect
+        with self.assertRaises(IntegrityError):
+            do_like(user.id, invalid_article_id)
+
+    def test_like_count_should_increase(self) -> None:
+        # given
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+
+        # when
+        do_like(user.id, article.id)
+
+        # then
+        result_article = Article.objects.get(id=article.id)
+        self.assertEqual(1, article.like_set.count())
+
+    def test_a_user_can_undo_like(self) -> None:
+        # given
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+        like = Like.objects.create(user_id=user.id, article_id=article.id)
+
+        # when
+        undo_like(user.id, article.id)
+
+        # then
+        with self.assertRaises(Like.DoesNotExist):
+            Like.objects.get(id=like.id)
