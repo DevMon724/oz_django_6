99379e97af0b20b94053451e093a225bbb5dbb6b
DevMon724 <devmon724@gmail.com>
2024-11-20 13:41:35 +0900

    do prefetch

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,12 +1,27 @@
-from django.db.models import QuerySet
+from django.db.models import Prefetch, QuerySet
 
-from tabom.models import Article
+from tabom.models import Article, Like
+
+# def get_an_article(article_id: int) -> Article:
+#
+#     return Article.objects.get(id=article_id)
+#
+#
+# def get_article_list(offset: int, limit: int) -> QuerySet[Article]:
+#     return Article.objects.order_by("-id")[offset : offset + limit]
 
 
-def get_an_article(article_id: int) -> Article:
+def get_an_article(user_id: int, article_id: int) -> Article:
+    return Article.objects.prefetch_related(
+        Prefetch("like_set", queryset=Like.objects.filter(user_id=user_id), to_attr="my_likes")
+    ).get(id=article_id)
 
-    return Article.objects.get(id=article_id)
 
-
-def get_article_list(offset: int, limit: int) -> QuerySet[Article]:
-    return Article.objects.order_by("-id")[offset : offset + limit]
+def get_article_list(user_id: int, offset: int, limit: int) -> QuerySet[Article]:
+    return (
+        Article.objects.order_by("-id")
+        .prefetch_related("like_set")
+        .prefetch_related(Prefetch("like_set", queryset=Like.objects.filter(user_id=user_id), to_attr="my_likes"))[
+            offset : offset + limit
+        ]
+    )
--- tabom/tests/test_article_service.py
+++ tabom/tests/test_article_service.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from tabom.models import Article, Like, User
 from tabom.services.article_service import get_an_article, get_article_list
+from tabom.services.like_service import do_like
 
 
 class TestArticleService(TestCase):
@@ -11,7 +12,7 @@ class TestArticleService(TestCase):
         article = Article.objects.create(title=title)
 
         # When
-        result_article = get_an_article(article.id)
+        result_article = get_an_article(0, article.id)
 
         # Then
         self.assertEqual(article.id, result_article.id)
@@ -23,21 +24,40 @@ class TestArticleService(TestCase):
 
         # Expect
         with self.assertRaises(Article.DoesNotExist):
-            get_an_article(invalid_article_id)
+            get_an_article(0, invalid_article_id)
 
-    def test_get_article_list_should_prefetch_likes(self) -> None:
+    # def test_get_article_list_should_prefetch_likes(self) -> None:
+    #     # Given
+    #     user = User.objects.create(name="user1")
+    #     articles = [Article.objects.create(title=f"{i}") for i in range(1, 21)]
+    #     Like.objects.create(user_id=user.id, article_id=articles[-1].id)
+    #
+    #     # When
+    #     result_articles = get_article_list(0, 10)
+    #
+    #     # Then
+    #     with self.assertNumQueries(2):
+    #
+    #         self.assertEqual(len(result_articles), 10)  # 길이가 10개가 맞는지 검증
+    #         self.assertEqual(1, result_articles[0].like_set.count())  #
+    #         self.assertEqual([a.id for a in reversed(articles[10:21])], [a.id for a in result_articles])
+    #         # assertEqual 은 순서도 비교 한다.
+
+    def test_get_article_list_should_prefetch_like(self) -> None:
         # Given
-        user = User.objects.create(name="user1")
+        user = User.objects.create(name="test_user")
         articles = [Article.objects.create(title=f"{i}") for i in range(1, 21)]
-        Like.objects.create(user_id=user.id, article_id=articles[-1].id)
+        do_like(user.id, articles[-1].id)
 
         # When
-        result_articles = get_article_list(0, 10)
+        with self.assertNumQueries(3):
+            result_articles = get_article_list(user.id, 0, 10)
+            result_counts = [a.like_set.count() for a in result_articles]
 
-        # Then
-        with self.assertNumQueries(2):
-
-            self.assertEqual(len(result_articles), 10)  # 길이가 10개가 맞는지 검증
-            self.assertEqual(1, result_articles[0].like_set.count())  #
-            self.assertEqual([a.id for a in reversed(articles[10:21])], [a.id for a in result_articles])
-            # assertEqual 은 순서도 비교 한다.
+            # Then
+            self.assertEqual(len(result_articles), 10)
+            self.assertEqual(1, result_counts[0])
+            self.assertEqual(
+                [a.id for a in reversed(articles[10:21])],
+                [a.id for a in result_articles],
+            )
